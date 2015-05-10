30d6db905e32332442fcd4bd63363b37b6dc7aea
Ramon Mulia <ramonmulia@gmail.com>
2015-05-10 00:04:47 -0300

    server em node

--- server.js
+++ server.js
@@ -1,46 +1,28 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+var express = require('express');
+var app = express();
+var path = require('path');
 
-http.createServer(function(request, response) {
+var port = process.env.PORT || 3000;
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
 
-  var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-  };
+console.log(__dirname);
 
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+app.use(express.static('bower_components'));
+app.use(express.static('app'));
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('common'));
 
-    if (fs.statSync(filename).isDirectory()) filename += '/views/index.html';
+app.get('/index', function(req, res) {
+	res.render('index.html');
+});
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
 
-      var headers = {};
-      var contentType = contentTypesByExtension[path.extname(filename)];
-      if (contentType) headers["Content-Type"] = contentType;
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
-
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+app.listen(port, function() {
+	console.log("listening on 3000");
+});
