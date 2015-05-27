912242439820dd8b49ffa1b89fefd69f029537fd
Ramon Mulia <ramonmulia@gmail.com>
2015-05-26 23:14:13 -0300

    product edit

---
+++ app/product/productEditCtrl.js
@@ -0,0 +1,17 @@
+(function() {
+	angular
+		.module("ProductManagement")
+		.controller("ProductCtrl", ["productResource", function(productResource) {
+			var vm = this;
+
+			productResource.query(function(data) {
+				vm.products = data
+			});
+
+			vm.showImage = false;
+
+			vm.toogleImage = function() {
+				vm.showImage = !vm.showImage;
+			};
+		}]);
+})();
